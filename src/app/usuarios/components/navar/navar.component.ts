602c387df9e7fd0cef68545f73e84127719fee04
nickollanos <nico26928@hotmail.com>
2024-12-17 11:13:19 -0500

    se realiza ajuste a la busqueda por medio del debouncer

--- src/app/usuarios/components/navar/navar.component.ts
+++ src/app/usuarios/components/navar/navar.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 
 import { UsuariosService } from '../../services/usuarios.service';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navar',
@@ -8,17 +9,42 @@ import { UsuariosService } from '../../services/usuarios.service';
   templateUrl: './navar.component.html',
   styleUrl: './navar.component.css'
 })
-export class NavarComponent {
+export class NavarComponent implements OnInit, OnDestroy{
+
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
+
+@Output()
+public onDebaunce =  new EventEmitter<string>();
 
 @ViewChild('txtUsuarioInput')
 public usuarioInput!: ElementRef<HTMLInputElement>;
 
 constructor( private usuariosService: UsuariosService ) {}
 
-searchUsuario() {
-  const newUsuario = this.usuarioInput.nativeElement.value;
+ngOnInit(): void {
+  this.debouncerSuscription = this.debouncer
+  .pipe(
+    debounceTime(1000)
+  )
+  .subscribe( value => {
+    // this.onDebaunce.emit( value );
+    this.usuariosService.searchUsuarios( value );
+  })
+  ;
+}
+
+ngOnDestroy(): void {
+  this.debouncerSuscription?.unsubscribe();
+}
+
+searchUsuario( term: string ) {
+
+  this.debouncer.next( term );
+
+  // const newUsuario = this.usuarioInput.nativeElement.value;
   //console.log({ newUsuario});
-  this.usuariosService.searchUsuarios(newUsuario);
+  // this.usuariosService.searchUsuarios(newUsuario);
   //this.usuarioInput.nativeElement.value = '';
 }
 
